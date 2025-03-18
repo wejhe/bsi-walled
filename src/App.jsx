9d280a49abbb6916428a1e13a2ca388916e5f1f5
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2025-03-18 12:52:47 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Link, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      {/* <input type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"/>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/login">Login</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes> */}
+    </>
+  );
+}
+
+export default App;
