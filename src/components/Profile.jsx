eff72e17417c79b5ad3687b1f986cd0f56233c4b
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2025-03-22 12:30:12 +0700

    fix : navbar always on top

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,7 +7,7 @@ const Profile = () => {
         </p>
         <p>Personal Account</p>
       </div>
-      <img className="profilepicture" src="https://cdn.discordapp.com/attachments/1351453135067156503/1351453591701032962/FOTO_ID_CARD_-_Copy.jpg?ex=67da6ebd&is=67d91d3d&hm=6ecddd15aa9a60c954129ced295fe871ac854500bc43541d7e718379c3204737&" />
+      <img className="profilepicture" src="https://cdn.discordapp.com/attachments/1351453135067156503/1351453591701032962/FOTO_ID_CARD_-_Copy.jpg?ex=67df0bfd&is=67ddba7d&hm=e0b324501fa9a42679bcc195ff363c464b615c8afe94f4f335c622fddf7162a2&" />
     </div>
   );
 };
