501e81eec9014ab63c85d40f3bf539e9dd0ffd86
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2025-03-22 12:56:17 +0700

    add : dynamic title using helmet

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -4,10 +4,14 @@ import Profile from "../components/Profile";
 import MainInfoLayout from "../layouts/MainInfoLayout";
 import AccountNumber from "../components/AccountNumber";
 import AccountInfo from "../components/AccountInfo";
+import { Helmet } from "react-helmet";
 
 const DashboardPage = () => {
   return (
     <>
+      <Helmet>
+        <title>BSI Walled &ndash; Dashboard</title>
+      </Helmet>
       <GreetingsLayout>
         <Greet />
         <Profile />
