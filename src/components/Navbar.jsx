227728008479ce622ee2896b22cf402a9ee9ea89
Yobato <satriyobagus30077@gmail.com>
2025-04-02 08:11:52 +0700

    fix: padding body + navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,28 +5,30 @@ const Navbar = () => {
   return (
     <>
       <nav>
-        <a className="clickableicon" href="/dashboard">
-          <img src={logo} />
-        </a>
-        <div className="menu">
-          <div>
-            <a className="navlink" href="/dashboard">
-              Dashboard
-            </a>
-            <a className="navlink" href="/topup">
-              Top-Up
-            </a>
-            <a className="navlink" href="/transfer">
-              Transfer
-            </a>
-            <a className="navlink" href="/">
-              Sign Out
+        <div className="nav-container">
+          <a className="clickableicon" href="/dashboard">
+            <img src={logo} />
+          </a>
+          <div className="menu">
+            <div>
+              <a className="navlink" href="/dashboard">
+                Dashboard
+              </a>
+              <a className="navlink" href="/topup">
+                Top-Up
+              </a>
+              <a className="navlink" href="/transfer">
+                Transfer
+              </a>
+              <a className="navlink" href="/">
+                Sign Out
+              </a>
+            </div>
+            <p className="navlink">|</p>
+            <a className="clickableicon" href="#">
+              <img src={darkmode} />
             </a>
           </div>
-          <p className="navlink">|</p>
-          <a className="clickableicon" href="#">
-            <img src={darkmode} />
-          </a>
         </div>
       </nav>
     </>
