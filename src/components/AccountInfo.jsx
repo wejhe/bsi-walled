eacfb77950ff751ec822a5540893bde1a2529105
Yobato <satriyobagus30077@gmail.com>
2025-04-02 09:15:53 +0700

    fix: Dashboard Page Component feat: hide-seek balance

--- src/components/AccountInfo.jsx
+++ src/components/AccountInfo.jsx
@@ -1,16 +1,34 @@
-import view from "/view.svg";
+// import view from "/view.svg";
 import topup from "/topup.png";
 import transfer from "/transfer.png";
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
 
 const AccountInfo = () => {
+  const [balanceVisible, setBalanceVisible] = useState(false);
+
+  const handleBalanceVisibility = () => {
+    setBalanceVisible(!balanceVisible);
+  };
   return (
     <div className="accinfo">
       <div className="accbalance">
         <p>Account Balance</p>
         <div className="balanceamount">
-          <h2>Rp980.572.000</h2>
-          <a className="clickableicon" href="#">
-            <img src={view} />
+          <strong className="info">
+            {/* Rp 980.572.000 */}
+            {balanceVisible ? "Rp 980.572.000" : "Rp **********"}
+          </strong>
+          <a
+            className="clickableicon"
+            href="#"
+            onClick={handleBalanceVisibility}
+          >
+            {balanceVisible ? (
+              <Eye size={32} color="#c0c0c0" />
+            ) : (
+              <EyeOff size={32} color="#c0c0c0" />
+            )}
           </a>
         </div>
       </div>
