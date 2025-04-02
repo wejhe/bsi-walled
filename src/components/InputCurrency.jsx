9fa5181107e58135971b17de3336999f00e783a5
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2025-04-02 23:51:43 +0700

    minor changes on InputCurrency component

--- src/components/InputCurrency.jsx
+++ src/components/InputCurrency.jsx
@@ -1,32 +1,36 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
-const InputCurrency = ({placeholder, width, onChange}) => {
-    const [value, setValue] = useState("");
+const InputCurrency = ({ placeholder, width, onChange }) => {
+  const [value, setValue] = useState("");
 
-    const formatCurrency = (input) =>{
-      const numericValue = input.replace(/\D/g, "");
-      return numericValue.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
+  const formatCurrency = (input) => {
+    const numericValue = input.replace(/\D/g, "");
+    return numericValue.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
 
-    const handleChange = (e) =>{
-        const rawValue = e.target.value;
-        const formatValue = formatCurrency(rawValue);
-        setValue(formatValue);
-        if (onChange) onChange(rawValue.replace(/,/g, ""));
-    }
+  const handleChange = (e) => {
+    const rawValue = e.target.value;
+    const formatValue = formatCurrency(rawValue);
+    setValue(formatValue);
+    if (onChange) onChange(rawValue.replace(/,/g, ""));
+  };
+
   return (
-    <input
-    style={{ width:width }}
-    className="inputFieldText"
-    type="text"
-    placeholder={placeholder}
-    value={value}
-    onChange={handleChange}
-    />
-      
-    
-  )
-}
+    <>
+      <div className="inputFieldCurrencyWrapper">
+        <input
+          style={{ width: width }}
+          className="inputFieldCurrency"
+          type="text"
+          placeholder={placeholder}
+          value={value}
+          onChange={handleChange}
+        />
+        <button className="wCurrencyIcon">Rp</button>
+      </div>
+    </>
+  );
+};
 
-export default InputCurrency
+export default InputCurrency;
