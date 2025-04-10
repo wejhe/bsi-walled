9f44f5f4ff9052bd49e3eb04a83aa3d567a266a6
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2025-04-10 20:52:44 +0700

    feat : implement api.js to use axios as fetching assistant

---
+++ src/utils/api.js
@@ -0,0 +1,88 @@
+import axios from "axios";
+import useAuthStore from "../stores/authStore";
+
+const api = axios.create({
+  baseURL: "http://localhost:8080",
+});
+
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+
+  failedQueue = [];
+};
+
+api.interceptors.request.use(
+  (config) => {
+    const { accessToken } = useAuthStore.getState();
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 403 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      const { refreshToken, setTokens, clearTokens } = useAuthStore.getState();
+
+      if (!refreshToken) {
+        clearTokens();
+        return Promise.reject(error);
+      }
+
+      if (isRefreshing) {
+        return new Promise(function (resolve, reject) {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers.Authorization = "Bearer " + token;
+            return api(originalRequest);
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      }
+
+      isRefreshing = true;
+
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/auth/refresh?refreshToken=${refreshToken}`
+        );
+
+        const { accessToken: newAccessToken, refreshToken: newRefreshToken } = response.data;
+        setTokens({ accessToken: newAccessToken, newRefreshToken });
+
+        api.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
+        processQueue(null, newAccessToken);
+        return api(originalRequest);
+      } catch (err) {
+        processQueue(err, null);
+        clearTokens();
+        return Promise.reject(err);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
